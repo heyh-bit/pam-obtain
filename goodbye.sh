f7dcd27dd4ffb12dad8de0592b618d3f667236eb
heyh-bit <Ying_Hui_He@163.com>
2024-09-26 14:20:06 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo "GITHUB_OUTPUT Before"
+cat $GITHUB_OUTPUT
 echo result=$(podman run --rm registry.cn-hangzhou.aliyuncs.com/hos_test/shterm-sdk:v8 | grep -v "Credential filePath") >> $GITHUB_OUTPUT
+cat $GITHUB_OUTPUT
+echo "GITHUB_OUTPUT After"
