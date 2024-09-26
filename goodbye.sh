99b0197ac9a7205dc8dbd06ac56e8761efc58222
heyh-bit <Ying_Hui_He@163.com>
2024-09-26 18:36:56 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -8,4 +8,7 @@ CONNECT_HOST="'$CONNECT_HOST'"
 CREDENTIAL_FILE="'$CREDENTIAL_FILE'"
 temp=$(echo "podman run --rm registry.cn-hangzhou.aliyuncs.com/hos_test/shterm-sdk:v8 $APP_ID $USER_NAME $ASSET_NAME $REQUEST_REASON $CONNECT_HOST $CREDENTIAL_FILE | grep -v 'Credential filePath'")
 success=$(eval $temp)
-echo result=$success
+echo username=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}')
+echo password=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $11}')
+echo ::add-mask::$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}')
+echo ::add-mask::$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $11}')
