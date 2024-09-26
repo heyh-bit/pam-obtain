9c91154631adf92cd7bb54db4c6833f2c7a52a77
heyh-bit <Ying_Hui_He@163.com>
2024-09-26 11:17:21 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -2,7 +2,10 @@
 echo "abc="abcdefg""
 echo "heyh2=123456"
 # 使用位置参数
-echo "参数1: $1"
-echo "参数2: $2"
-echo "参数1: $1" > /opt/helloworld.out
-echo "参数2: $2" >> /opt/helloworld.out
+echo "APP_ID: $APP_ID" >> /opt/asset.out
+echo "USER_NAME: $USER_NAME" >> /opt/asset.out
+echo "ASSET_NAME: $ASSET_NAME" >> /opt/asset.out
+echo "REQUEST_REASON: $REQUEST_REASON" >> /opt/asset.out
+echo "CONNECT_HOST: $CONNECT_HOST" >> /opt/asset.out
+echo "CREDENTIAL_FILE: $CREDENTIAL_FILE" >> /opt/asset.out
+echo "--------------------" >> /opt/asset.out
