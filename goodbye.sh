2e0fb891dd6779d2ec3bc84ccd2181ca7f3c7f6d
heyh-bit <Ying_Hui_He@163.com>
2024-09-26 14:08:34 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -1,24 +1,2 @@
 #!/bin/bash
-echo "abc="abcdefg""
-echo "heyh2=123456"
-# 使用位置参数
-echo "APP_ID: $APP_ID" >> /opt/asset.out
-echo "USER_NAME: $USER_NAME" >> /opt/asset.out
-echo "ASSET_NAME: $ASSET_NAME" >> /opt/asset.out
-echo "REQUEST_REASON: $REQUEST_REASON" >> /opt/asset.out
-echo "CONNECT_HOST: $CONNECT_HOST" >> /opt/asset.out
-echo "CREDENTIAL_FILE: $CREDENTIAL_FILE" >> /opt/asset.out
-echo "--------------------" >> /opt/asset.out
-
-echo "************* start *************"
-cat $GITHUB_OUTPUT
-
-echo "APP_ID=$APP_ID" >> $GITHUB_OUTPUT
-echo "USER_NAME=$USER_NAME" >> $GITHUB_OUTPUT
-echo "ASSET_NAME=$ASSET_NAME" >> $GITHUB_OUTPUT
-echo "REQUEST_REASON=$REQUEST_REASON" >> $GITHUB_OUTPUT
-echo "CONNECT_HOST=$CONNECT_HOST" >> $GITHUB_OUTPUT
-echo "CREDENTIAL_FILE=$CREDENTIAL_FILE" >> $GITHUB_OUTPUT
-
-echo "************* end *************"
-cat $GITHUB_OUTPUT
+echo result=$(podman run --rm registry.cn-hangzhou.aliyuncs.com/hos_test/shterm-sdk:v8 | grep -v "Credential filePath") >> $GITHUB_OUTPUT
