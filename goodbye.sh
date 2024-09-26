122beba38420f9e8b754fcf462b340cc982977f4
heyh-bit <Ying_Hui_He@163.com>
2024-09-26 15:02:29 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo result=$(podman run --rm registry.cn-hangzhou.aliyuncs.com/hos_test/shterm-sdk:v8 | grep -v "Credential filePath")
+echo result=$(podman run --rm registry.cn-hangzhou.aliyuncs.com/hos_test/shterm-sdk:v8  $APP_ID $USER_NAME $ASSET_NAME $REQUEST_REASON $CONNECT_HOST $CREDENTIAL_FILE | grep -v 'Credential filePath')
