1e14ba8a8a3d9635d6c48d2f166307882b3a25cd
heyh-bit <Ying_Hui_He@163.com>
2024-09-26 17:11:28 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -6,9 +6,10 @@ REQUEST_REASON="'$REQUEST_REASON'"
 CONNECT_HOST="'$CONNECT_HOST'"
 CREDENTIAL_FILE="'$CREDENTIAL_FILE'"
 echo $APP_ID > /opt/args.out
-echo $USER_NAME > /opt/args.out
-echo $ASSET_NAME > /opt/args.out
-echo $REQUEST_REASON > /opt/args.out
-echo $CONNECT_HOST > /opt/args.out
-echo $CREDENTIAL_FILE > /opt/args.out
+echo $USER_NAME >> /opt/args.out
+echo $ASSET_NAME >> /opt/args.out
+echo $REQUEST_REASON >> /opt/args.out
+echo $CONNECT_HOST >> /opt/args.out
+echo $CREDENTIAL_FILE >>/opt/args.out
+echo "podman run --rm registry.cn-hangzhou.aliyuncs.com/hos_test/shterm-sdk:v8 $APP_ID $USER_NAME $ASSET_NAME $REQUEST_REASON $CONNECT_HOST $CREDENTIAL_FILE | grep -v 'Credential filePath'" >> /opt/args.out
 echo result=$(podman run --rm registry.cn-hangzhou.aliyuncs.com/hos_test/shterm-sdk:v8 $APP_ID $USER_NAME $ASSET_NAME $REQUEST_REASON $CONNECT_HOST $CREDENTIAL_FILE | grep -v 'Credential filePath')
