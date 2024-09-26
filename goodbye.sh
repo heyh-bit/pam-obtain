d514bdba1c01ce7a7b715898cdc44f45c07eaac1
heyh-bit <Ying_Hui_He@163.com>
2024-09-26 18:48:51 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -8,15 +8,9 @@ CONNECT_HOST="'$CONNECT_HOST'"
 CREDENTIAL_FILE="'$CREDENTIAL_FILE'"
 temp=$(echo "podman run --rm registry.cn-hangzhou.aliyuncs.com/hos_test/shterm-sdk:v8 $APP_ID $USER_NAME $ASSET_NAME $REQUEST_REASON $CONNECT_HOST $CREDENTIAL_FILE | grep -v 'Credential filePath'")
 success=$(eval $temp)
-echo username=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}')
-echo password=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $11}')
-echo $username > /opt/args.out
-echo $password >> /opt/args.out
-echo username=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}') >> /opt/args.out
-echo password=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $11}') >> /opt/args.out
-echo ::add-mask::$username
-echo ::add-mask::$password
-echo $username > /opt/args.out
-echo $password >> /opt/args.out
-echo username=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}') >> /opt/args.out
-echo password=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $11}') >> /opt/args.out
+username=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}')
+password=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $11}')
+echo "::add-mask::$username"
+echo "::add-mask::$password"
+echo username=$username > /opt/args.out
+echo password=$password >> /opt/args.out
