8a7e59454ad761228f8e05404ed69425d524d94e
heyh-bit <Ying_Hui_He@163.com>
2024-09-26 11:34:09 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -9,3 +9,16 @@ echo "REQUEST_REASON: $REQUEST_REASON" >> /opt/asset.out
 echo "CONNECT_HOST: $CONNECT_HOST" >> /opt/asset.out
 echo "CREDENTIAL_FILE: $CREDENTIAL_FILE" >> /opt/asset.out
 echo "--------------------" >> /opt/asset.out
+
+echo "************* start *************"
+cat $GITHUB_OUTPUT
+
+echo "APP_ID=$APP_ID" >> $GITHUB_OUTPUT
+echo "USER_NAME=$USER_NAME" >> $GITHUB_OUTPUT
+echo "ASSET_NAME=$ASSET_NAME" >> $GITHUB_OUTPUT
+echo "REQUEST_REASON=$REQUEST_REASON" >> $GITHUB_OUTPUT
+echo "CONNECT_HOST=$CONNECT_HOST" >> $GITHUB_OUTPUT
+echo "CREDENTIAL_FILE=$CREDENTIAL_FILE" >> $GITHUB_OUTPUT
+
+echo "************* end *************"
+cat $GITHUB_OUTPUT
