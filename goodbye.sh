8fcbaeb6240f16f821682ab315fd2dd9c3337f41
heyh-bit <Ying_Hui_He@163.com>
2024-09-26 18:13:04 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Args Process
+APP_ID="'$APP_ID'"
+USER_NAME="'$USER_NAME'"
+ASSET_NAME="'$ASSET_NAME'"
+REQUEST_REASON="'$REQUEST_REASON'"
+CONNECT_HOST="'$CONNECT_HOST'"
+CREDENTIAL_FILE="'$CREDENTIAL_FILE'"
 temp=$(echo "podman run --rm registry.cn-hangzhou.aliyuncs.com/hos_test/shterm-sdk:v1 $APP_ID $USER_NAME $ASSET_NAME $REQUEST_REASON $CONNECT_HOST $CREDENTIAL_FILE | grep -v 'Credential filePath'")
 success=$(eval $temp)
 echo result=$success
