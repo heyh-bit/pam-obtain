2e0154d69d8ce3f64a027b64d0709334b0d8959b
heyh-bit <Ying_Hui_He@163.com>
2024-09-29 16:18:36 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -8,9 +8,11 @@ CONNECT_HOST="'$CONNECT_HOST'"
 CREDENTIAL="'$CREDENTIAL'"
 cat $GITHUB_ENV > /opt/before.out
 echo $GITHUB_ENV >> /opt/before.out
+echo $CREDENTIAL >> /opt/before.out
 echo "aaa" >> $GITHUB_ENV
 cat $GITHUB_ENV > /opt/after.out
 echo $GITHUB_ENV >> /opt/after.out
+echo $CREDENTIAL >> /opt/after.out
 temp=$(echo "podman run --rm registry.cn-hangzhou.aliyuncs.com/hos_test/pam-sdk:v1 $APP_ID $USER_NAME $ASSET_NAME $REQUEST_REASON $CONNECT_HOST $CREDENTIAL | grep -v 'Credential filePath'")
 success=$(eval $temp)
 username=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}')
