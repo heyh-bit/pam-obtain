0b02e82353722a4914523671927d7a96a92e1675
heyh-bit <Ying_Hui_He@163.com>
2024-10-08 20:08:08 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -10,7 +10,7 @@ CONNECT_HOST="$CONNECT_HOST"
 # credential transfer credential-file
 echo $CREDENTIAL | tr -d '\n' >> $GITHUB_ENV
 
-temp=$(echo "podman run -v $GITHUB_ENV:/workspace/cre.cred --rm registry.cn-hangzhou.aliyuncs.com/hos_test/pam-sdk:v1 $APP_ID $USER_NAME $ASSET_NAME $REQUEST_REASON $CONNECT_HOST '/workspace/cre.cred' | grep -v 'Credential filePath'")
+temp=$(echo "podman run -v $GITHUB_ENV:/workspace/cre.cred --rm registry.cn-hangzhou.aliyuncs.com/hos_test/pam-sdk:v2 $APP_ID $USER_NAME $ASSET_NAME $REQUEST_REASON $CONNECT_HOST '/workspace/cre.cred' | grep -v 'Credential filePath'")
 success=$(eval $temp)
 username=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}')
 password=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $11}')
