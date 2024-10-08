b6057c385cbfc19ad8964720705eaec82f96ca74
heyh-bit <Ying_Hui_He@163.com>
2024-10-08 11:03:54 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -14,6 +14,6 @@ temp=$(echo "podman run -v $GITHUB_ENV:/workspace/cre.cred --rm registry.cn-hang
 success=$(eval $temp)
 username=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}')
 password=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $11}')
-ENCRYPTED_PASSWORD=$(openssl enc -aes-256-cbc -pass pass:YOUR_ENCRYPTION_PASSPHRASE -base64 <<< "$password")
+ENCRYPTED_PASSWORD=$(openssl enc -aes-256-cbc -pass pass:YOUR_ENCRYPTION_PASSPHRASE -pbkdf2 -base64 <<< "$password")
 echo username=$username
 echo password=$ENCRYPTED_PASSWORD
