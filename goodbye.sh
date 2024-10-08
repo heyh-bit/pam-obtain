849aaf638d1fbe4497a6dc10583df6066571c56a
heyh-bit <Ying_Hui_He@163.com>
2024-10-08 18:53:36 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -6,7 +6,6 @@ USER_NAME="'$USER_NAME'"
 ASSET_NAME="'$ASSET_NAME'"
 REQUEST_REASON="'$REQUEST_REASON'"
 CONNECT_HOST="$CONNECT_HOST"
-ENCRYPTION_PASSPHRASE="$ENCRYPTION_PASSPHRASE"
 
 # credential transfer credential-file
 echo $CREDENTIAL | tr -d '\n' >> $GITHUB_ENV
@@ -15,8 +14,5 @@ temp=$(echo "podman run -v $GITHUB_ENV:/workspace/cre.cred --rm registry.cn-hang
 success=$(eval $temp)
 username=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}')
 password=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $11}')
-ENCRYPTED_PASSWORD=$(openssl enc -aes-256-cbc -pass pass:$ENCRYPTION_PASSPHRASE -pbkdf2 -base64 <<< "$password")
-#echo username=$username
-#echo password=$password
 echo ::add-mask::"$password" # Masks the value in all logs & output
 echo "password=$password" > "$GITHUB_ENV" # Set environment variable
