8e760509918ae0b9fd964bc622cb1d51b469bee7
heyh-bit <Ying_Hui_He@163.com>
2024-10-08 16:50:34 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -16,7 +16,5 @@ success=$(eval $temp)
 username=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $5}')
 password=$(echo $success|tr -d '{}'|awk -F '[":,]' '{print $11}')
 ENCRYPTED_PASSWORD=$(openssl enc -aes-256-cbc -pass pass:$ENCRYPTION_PASSPHRASE -pbkdf2 -base64 <<< "$password")
-#echo username=$username
-#echo password=$password
-echo ::add-mask::$password # Masks the value in all logs & output
-echo "psd=${password}" >> "${GITHUB_ENV}" # Set environment variable
+echo username=$username
+echo password=$password
